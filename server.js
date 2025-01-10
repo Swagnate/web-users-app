f3a4fa740b8b43080fb914e7e0ac23c0cc5da6cf
Matveev matvei <matthew.marinara@gmail.com>
2025-01-10 16:44:20 +0300

    adding a comment

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
+// Импорт модулей
 const express = require('express');
 const path = require('path');
 const { sequelize } = require('./models');
-const routes = require('./routes');
+const routes = require('./routes'); // подключаем маршруты
 const { url } = require('inspector');
 
 const app = express()
 
+// Устанавливаем порт для сервера.
 const PORT = 3000 || process.env.PORT
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Middleware для парсинга JSON
+// Middleware для парсинга данных
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', routes);
 
-sequelize.sync({ force: false })
+// Синхронизация базы данных с использованием Sequelize
+sequelize.sync({ force: false }) // force: false - таблицы не будут созданы, если уже существуют
     .then(() => {
         console.log('Database synced!');
+        // Запускаем сервер и слушаем указанный порт
         app.listen(PORT, () => {
             console.log(`Server started on http://localhost:${PORT}`);
         });
     })
     .catch((err) => {
-        console.error('Error syncing database:', err);
+        console.error('Error syncing database:', err); // Обработка ошибок
     });
